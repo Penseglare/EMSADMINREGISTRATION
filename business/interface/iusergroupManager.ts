84f0f2c89ae9e87e0fc528da6b0ed7558f0ad3a7
Penseglare <penseglare@gmail.com>
2020-03-01 17:42:28 +0530

    updated

--- business/interface/iusergroupManager.ts
+++ business/interface/iusergroupManager.ts
@@ -5,7 +5,7 @@ export default interface iusergroupManager
     getUserGroup():any;   
     getUserGroupById(pkId:string):any;
     deleteUserGroup(pkId:string):any;
-    saveUserGroup(registration:any):any
-    updateUserGroup(registration:any,id:string):any;
+    saveUserGroup(usergroup:any):any
+    updateUserGroup(usergroup:any,id:string):any;
 
 }
